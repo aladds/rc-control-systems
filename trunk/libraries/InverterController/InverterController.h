523042a090c24c447fd91f7662b40884adfe51e5
AlexJBarber@gmail.com <AlexJBarber@gmail.com@0e6db2ac-0183-1669-dfd1-8423bfcb7808>
2014-04-05 08:29:02 +0000

    First Commit. InverterController Library and Base arduino code.

---
+++ trunk/libraries/InverterController/InverterController.h
@@ -0,0 +1,16 @@
+#ifndef InverterController_H
+#define InverterController_H
+
+#include <Arduino.h>
+
+class InverterController
+{
+public:
+	InverterController(byte forwardPin, byte reversePin, byte midPin, byte highPin, byte midPin2, byte highPin2);
+	void driveForward();
+	void driveReverse();
+	void coast();
+	void setSpeed(int speed);
+};
+
+#endif
